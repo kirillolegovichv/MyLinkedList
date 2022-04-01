d8a15050c2c9490e90d00896410b39b3dbc0403c
Kirill <kirill.vasil.oleg@gmail.com>
2022-04-01 20:09:35 +0300

    Add LinkedListClass

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -1,7 +1,40 @@
 ﻿namespace LinkedList
 {
-    public class Class1
+    public class LinkedList
     {
+        private Node _head;
 
+        private Node _tail;
+
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        public LinkedList(int value)
+        {
+            _head = new Node(value);
+            _tail = _head;
+        }
+
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+                Node crnt = _head;
+                while (crnt != null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+                return count;
+            }
+            private set
+            {
+
+            }
+        }
     }
 }
