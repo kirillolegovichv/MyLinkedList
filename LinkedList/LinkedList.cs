7b6ab2502b7c18ec0703f388567e66816645b3e0
Kirill <kirill.vasil.oleg@gmail.com>
2022-04-02 12:57:11 +0300

    Add methods and tests

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -48,11 +48,10 @@
             {
                 _head = new Node(values[0]);
                 _tail = _head;
-                Node crnt = _head;
+                
                 for (int i = 1; i < values.Length; i++)
                 {
-                    crnt.Next = new Node(values[i]);
-                    _tail = crnt;
+                    Add(values[i]);
                 }
 
             }
@@ -71,6 +70,109 @@
                 _tail = _tail.Next;
 
             }
+        }
+
+        public void AddToStart(int value)   
+        {
+            if (_head == null)
+            {
+                _head = new Node(value);
+                _tail = _head;
+            }
+            else
+            {
+                Node crnt = _head;
+                _head = new Node(value);
+                _head.Next = crnt;
+            }
+        }
+
+        public void AddByIndex(int index, int value)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new ArgumentException("index is wrong");
+            }
+            else if (index == 0)
+            {
+                AddToStart(value);
+            }
+            else
+            {
+                Node prev = GetNodeByIndex(index - 1);
+                Node next = GetNodeByIndex(index);
+                Node newNode = new Node(value);
+                
+                newNode.Next = next;
+                prev.Next = newNode;
+            }
+        }
+
+        public void Pop()
+        {
+            if (Length == 0)
+            {
+                throw new Exception("List already empty!");
+            }
+            if (Length == 1)
+            {
+                _head = null;
+                _tail = null;
+            }
+            else
+            {
+                Node prev = GetNodeByIndex(Length - 2);
+                prev.Next = null;
+                _tail = prev;
+            }
+
+        }
+
+        public void PopFromStart()
+        {
+            if (Length == 0)
+            {
+                throw new Exception("List already empty!");
+            }
+            _head = _head.Next;
+        }
+
+        public void PopByIndex(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new Exception("Index out of range");
+            }
+            if (index == 0)
+            {
+                PopFromStart();
+            }
+            else
+            {
+                Node prev = GetNodeByIndex(index - 1);
+                Node next = GetNodeByIndex(index + 1);
+
+                prev.Next = next;
+            }
+            
+        }
+
+        public void PopElems(int count)
+        {
+            if (count > Length)
+            {
+                throw new ArgumentException("count is wrong, count mustn't be higher than Length");
+            }
+            while (count != 0)
+            {
+                Pop();
+                count--;
+            }
+        }
+
+        public void PopElemsFromStart(int count)
+        {
+
         }
 
         public override string ToString()
@@ -120,6 +222,18 @@
                 }
             }
             return isEqual;
+        }
+
+        private Node GetNodeByIndex(int index)
+        {
+            Node crnt = _head;
+
+            for (int i = 1; i <= index; i++)
+            {
+                crnt = crnt.Next;
+            }
+
+            return crnt;
         }
 
     }
--- MyLinkedList.Test/LinkedListTest.cs
+++ MyLinkedList.Test/LinkedListTest.cs
@@ -13,16 +13,130 @@ namespace MyLinkedList.Test
             actualList.Add(value);
             Assert.AreEqual(expectedList, actualList);
         }
+
+        [TestCaseSource(typeof(AddToStartTestSource))]
+        public void AddToStartTest(int value, LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.AddToStart(value);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(AddByIndexTestSource))]
+        public void AddByIndexTest(int index, int value, LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.AddByIndex(index, value);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(PopTestSource))]
+        public void PopTest(LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.Pop();
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(PopFromStartTestSource))]
+        public void PopFromStartTest(LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.PopFromStart();
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(PopByIndexTestSource))]
+        public void PopByIndexTest(int index, LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.PopByIndex(index);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(PopElemsTestSource))]
+        public void PopElemsTest(int count, LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.PopElems(count);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(PopElemsFromStartTestSource))]
+        public void PopElemsFromStartTest(int count, LinkedList list, LinkedList expectedList)
+        {
+            LinkedList actualList = list;
+            actualList.PopElemsFromStart(count);
+            Assert.AreEqual(expectedList, actualList);
+        }
     }
 
     public class AddTestSource : IEnumerable
     {
         public IEnumerator GetEnumerator()
         {
-            int value = 0;
-            LinkedList list = new LinkedList(new int[] { 1, 2, 3 });
-            LinkedList expectedList = new LinkedList(new int[] { 1, 2, 3, 0 });
-            yield return new object[]  { value, list, expectedList };
+            yield return new object[] { 5, new LinkedList(new int[] { 3, 4 }), new LinkedList(new int[] { 3, 4, 5 }) };
+            yield return new object[] { 2, new LinkedList(new int[] { 1, 4, 3 }), new LinkedList(new int[] { 1, 4, 3, 2 }) };
+        }
+    }
+
+    public class AddToStartTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 2, new LinkedList(new int[] { 3, 4 }), new LinkedList(new int[] { 2, 3, 4 }) };
+            yield return new object[] { 5, new LinkedList(new int[] { 1, 4, 3 }), new LinkedList(new int[] { 5, 1, 4, 3 }) };
+        }
+    }
+
+    public class AddByIndexTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 2, 4, new LinkedList(new int[] { 1, 2, 3 }), new LinkedList(new int[] { 1, 2, 4, 3 }) };
+        }
+    }
+
+    public class PopTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] {new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 1, 2, 3 }) };
+            yield return new object[] {new LinkedList(new int[] { 2 }), new LinkedList(new int[] { }) };
+        }
+    }
+
+    public class PopFromStartTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 2, 3, 4 }) };
+        }
+    }
+
+    public class PopByIndexTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] {2, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 1, 2, 4 }) };
+            yield return new object[] {0, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 2, 3, 4 }) };        }
+    }
+
+    public class PopElemsTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 2, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 1, 2 }) };
+            yield return new object[] { 0, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 1, 2, 3, 4 }) };
+        }
+    }
+
+    public class PopElemsFromStartTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 2, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 3, 4 }) };
+            yield return new object[] { 0, new LinkedList(new int[] { 1, 2, 3, 4 }), new LinkedList(new int[] { 1, 2, 3, 4 }) };
         }
     }
 }
